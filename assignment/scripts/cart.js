4bc74bb17b28eb790579ca33bd4d4d4c7dbafda3
Dylan Roets <dylanroets@yahoo.com>
2022-10-16 21:57:08 -0500

    I think I got it, yaaay

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,14 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem(item){
+    console.log('in addItem', item);
+    basket.push(item);
+    return true;
+}
+console.log('Test adding fruit', addItem('apple'));
+addItem('banana');
+console.log(basket);
+addItem('kiwi')
+console.log(basket);
