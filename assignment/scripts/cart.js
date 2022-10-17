bd2561748318351f541a1f1185c37043ff120213
Dylan Roets <dylanroets@yahoo.com>
2022-10-16 22:34:57 -0500

    syntax errors suck

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -10,5 +10,22 @@ function addItem(item){
 console.log('Test adding fruit', addItem('apple'));
 addItem('banana');
 console.log(basket);
-addItem('kiwi')
+addItem('kiwi');
 console.log(basket);
+addItem('lemon');
+console.log(addItem('jackfruit'));
+console.log(basket);
+console.log('Adding apples (expect true)', addItem('apples'));
+//end testing for addItem
+
+//start listItems
+function listItems(basket){
+    for (item of basket){
+        console.log('Basket Item: ', item);
+    }
+    return 'Those are all of the items'
+    
+}
+console.log(listItems(basket));
+//not sure why im getting an undefined after the list
+//nevermind have to do a return in the function
